3d1d989773ab54b8af5c1c900f3b3debd15590d6
mouadelfaiz <44578535+mouadelfaiz@users.noreply.github.com>
2023-08-08 16:55:59 -0400

    first commit

--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 // Menu data structure
 var menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '/catalog'},
-    {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
-  ];
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
 const maiEl = document.querySelector("main")
 
 maiEl.style.backgroundColor = "var(--main-bg)"
@@ -29,3 +40,75 @@ menuLinks.forEach((element) => {
     topMenuEl.appendChild(alink);
 })
 
+const subMenuEl = document.querySelector("#sub-menu")
+
+subMenuEl.style.height = "100%"
+
+subMenuEl.style.backgroundColor = "var(--sub-menu-bg)"
+
+subMenuEl.classList.add("flex-around")
+
+subMenuEl.style.position = "absolute"
+
+subMenuEl.style.top = "0"
+
+const topMenuLinks = document.querySelectorAll("a")
+
+showingSubMenu = false;
+
+topMenuEl.addEventListener("click", (event) => {
+  event.preventDefault()
+  console.log(event.target.tagName)
+
+  topMenuLinks.forEach(link => {
+    if (link.textContent !== event.target.textContent) {
+      link.classList.remove("active")
+      showingSubMenu = false;
+      subMenuEl.style.top = "0";
+    } else {
+      link.classList.add("active")
+    }
+  })
+  menuLinks.forEach(element => {
+    if (element.text !== "about") {
+      showingSubMenu = true 
+    } else {
+      showingSubMenu = false
+    }
+  })
+}
+)
+function buildSubMenu(subLinksArray) {
+  subMenuEl.innerHTML = '';
+
+  subLinksArray.forEach(linkObj => {
+    const subLink = document.createElement('a');
+    subLink.setAttribute('href', linkObj.href);
+    subLink.textContent = linkObj.text;
+    subMenuEl.appendChild(subLink);
+  });
+}
+subMenuEl.addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.tagName !== 'A') return;
+  console.log(event.target.textContent); 
+});
+subMenuEl.addEventListener('click', event => {
+  event.preventDefault(); 
+
+  if (event.target.tagName !== 'A') return;
+
+  showingSubMenu = false;
+  subMenuEl.style.top = '0';
+
+  topMenuLinks.forEach(link => {
+    link.classList.remove('active');
+  });
+
+  const clickedLinkText = event.target.textContent;
+  maiEl.innerHTML = `<h1>${clickedLinkText}</h1>`;
+
+  if (clickedLinkText === 'about') {
+    maiEl.innerHTML = '<h1>about</h1>';
+  }
+});
